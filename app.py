ca4ba68039e459ab63cc4343116f49aba7fc4852
naveen-annap <76947897+naveen-annap@users.noreply.github.com>
2021-04-26 12:20:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,28 +5,28 @@ from flask import Flask, request, abort
 
 
 def retrievesheet():
-	import gspread
-	from oauth2client.client import GoogleCredentials as GC
-	from gspread_dataframe import get_as_dataframe, set_with_dataframe
-	from datetime import datetime
-	import pandas as pd
-	#gc = gspread.authorize(GC.get_application_default())
-	from oauth2client.service_account import ServiceAccountCredentials
-	scope = ['https://spreadsheets.google.com/feeds',
-	'https://www.googleapis.com/auth/drive']
-         credentials = ServiceAccountCredentials.from_json_keyfile_name(environ["GOOGLE_SHEETS_CREDS_JSON"] , scope)
-     gc = gspread.authorize(credentials)
-     master_sh=gc.open_by_key("1JTJlCdD1k96WUxkxuBq7OU7btBZqKxny9x8p9lo5IU0")
-     master_worksheet = master_sh.worksheet("Sheet2")
-     dfkeys = pd.DataFrame(master_worksheet.get_all_values()[1:11])
-     print(dfkeys.columns)
+    import gspread
+    from oauth2client.client import GoogleCredentials as GC
+    from gspread_dataframe import get_as_dataframe, set_with_dataframe
+    from datetime import datetime
+    import pandas as pd
+    #gc = gspread.authorize(GC.get_application_default())
+    from oauth2client.service_account import ServiceAccountCredentials
+    scope = ['https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(environ["GOOGLE_SHEETS_CREDS_JSON"] , scope)
+    gc = gspread.authorize(credentials)
+    master_sh=gc.open_by_key("1JTJlCdD1k96WUxkxuBq7OU7btBZqKxny9x8p9lo5IU0")
+    master_worksheet = master_sh.worksheet("Sheet2")
+    dfkeys = pd.DataFrame(master_worksheet.get_all_values()[1:11])
+    print(dfkeys.columns)
 
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     #retrievesheet()
-	print("123")
+    print("123")
     if request.method == 'POST':
         print(request.json)
         return 'success', 200
