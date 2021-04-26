d7e2871626d910040f8b0b2ae963ab2abf2ddcaa
apvkarthick <76872683+apvkarthick@users.noreply.github.com>
2021-04-26 12:11:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-import gspread
-from oauth2client.client import GoogleCredentials as GC
-from gspread_dataframe import get_as_dataframe, set_with_dataframe
-from datetime import datetime
-import pandas as pd
-#gc = gspread.authorize(GC.get_application_default())
-from oauth2client.service_account import ServiceAccountCredentials
+
 from os import environ
 from flask import Flask, request, abort
 #'O:\\downloads-nov-2018\\python-self-programs\\akarthick-sheets-api.json'
 
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name(environ["GOOGLE_SHEETS_CREDS_JSON"] , scope)
+
 def retrievesheet():
+	import gspread
+	from oauth2client.client import GoogleCredentials as GC
+	from gspread_dataframe import get_as_dataframe, set_with_dataframe
+	from datetime import datetime
+	import pandas as pd
+	#gc = gspread.authorize(GC.get_application_default())
+	from oauth2client.service_account import ServiceAccountCredentials
+	scope = ['https://spreadsheets.google.com/feeds',
+	'https://www.googleapis.com/auth/drive']
+         credentials = ServiceAccountCredentials.from_json_keyfile_name(environ["GOOGLE_SHEETS_CREDS_JSON"] , scope)
      gc = gspread.authorize(credentials)
      master_sh=gc.open_by_key("1JTJlCdD1k96WUxkxuBq7OU7btBZqKxny9x8p9lo5IU0")
      master_worksheet = master_sh.worksheet("Sheet2")
@@ -23,7 +25,7 @@ def retrievesheet():
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def index():
-    retrievesheet()
+    #retrievesheet()
     if request.method == 'POST':
         print(request.json)
         return 'success', 200
